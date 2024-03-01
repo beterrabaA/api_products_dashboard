07e9eb9b58451f03ed73fd26783bc8fb7366c4f1
Beterraba <ls6182315@gmail.com>
2024-03-01 19:54:41 -0300

    validate user login

--- src/useCases/user.usecase.js
+++ src/useCases/user.usecase.js
@@ -26,4 +26,15 @@ module.exports = class UserUseCase {
 
     return user;
   }
+
+  async login(email, password) {
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) throw new Error("user not found");
+
+    const validatedPassword = hashDecode(password, user.password);
+    if (!validatedPassword) throw new Error("password invalid");
+
+    return user;
+  }
 };
