86c60dd6307c3a1f3297cdb57f502da7dbd495d0
Beterraba <ls6182315@gmail.com>
2024-03-01 14:31:40 -0300

    product model

---
+++ src/models/product.model.js
@@ -0,0 +1,26 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define(
+    "Product",
+    {
+      name: { type: DataTypes.STRING, allowNull: false },
+      brand: DataTypes.STRING,
+      model: DataTypes.STRING,
+      data: {
+        type: DataTypes.ARRAY(DataTypes.JSON),
+        defaultValue: [],
+      },
+      userId: { type: DataTypes.UUID, foreignKey: true },
+    },
+    {
+      timestamps: true,
+      tableName: "products",
+      underscored: true,
+    }
+  );
+
+  Product.associate = (models) => {
+    Product.belongsTo(models.User, { foreignKey: "userId", as: "users" });
+  };
+
+  return Product;
+};
