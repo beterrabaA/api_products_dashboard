ab4f4577f6b3496534a1f49644b56ad0ed6fd81e
Beterraba <ls6182315@gmail.com>
2024-03-01 19:24:35 -0300

    fix error messages

--- src/middlewares/credentials.middleware.js
+++ src/middlewares/credentials.middleware.js
@@ -4,11 +4,13 @@ module.exports = class Credentials {
 
     const emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
     if (email === "" || !emailFormat.test(email)) {
-      return res.status(401).json({ message: "Invalid email" });
+      return res.status(401).json({ message: "invalid email format" });
     }
 
     if (password.length < 6) {
-      return res.status(401).json({ message: "Invalid password" });
+      return res
+        .status(401)
+        .json({ message: "password must be more than 6 characters" });
     }
     next();
   };
