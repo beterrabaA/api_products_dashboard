ba577ad730c77e9f54a7d90342e8cc47b7bec26a
Beterraba <ls6182315@gmail.com>
2024-03-01 14:31:28 -0300

    create user controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,22 @@
+const UserUseCase = require("../useCases/user.usecase.js");
+
+export class UserController {
+  constructor() {
+    this.usecase = new UserUseCase();
+  }
+
+  async create(req, res, next) {
+    const { username, email, password } = req.body;
+    try {
+      const createdUser = await this.usecase.createUser(
+        username,
+        email,
+        password
+      );
+
+      return res.status(200).json(createdUser);
+    } catch (error) {
+      res.status(409).json({ message: "User already registered" });
+    }
+  }
+}
