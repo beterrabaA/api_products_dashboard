34ca056d65865c190d19d06d61966e3ed412d699
Beterraba <ls6182315@gmail.com>
2024-03-01 19:11:43 -0300

    create credentials middleware validator

---
+++ src/middlewares/credentials.middleware.js
@@ -0,0 +1,15 @@
+module.exports = class Credentials {
+  validateCredentials = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    const emailFormat = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/;
+    if (email === "" || !emailFormat.test(email)) {
+      return res.status(401).json({ message: "Invalid email" });
+    }
+
+    if (password.length < 6) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    next();
+  };
+};
