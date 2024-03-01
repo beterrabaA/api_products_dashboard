c0e12d384a260d7bc58a5aef0f8baf4130922d74
Beterraba <ls6182315@gmail.com>
2024-03-01 14:56:57 -0300

    fix export module

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -1,13 +1,20 @@
 const crypto = require("crypto");
 
+require("dotenv/config");
+
 const CRYPTO_SALT = process.env.CRYPTO_SALT || "salt";
 
-export const hashEncode = (password) => {
+const hashEncode = (password) => {
   return crypto
     .pbkdf2Sync(password, CRYPTO_SALT, 10000, 12, "sha512") // Ajuste o algoritmo e os parâmetros conforme necessário
     .toString("hex");
 };
 
-export const hashDecode = (password, hash) => {
+const hashDecode = (password, hash) => {
   return hashEncode(password) === hash;
+};
+
+module.exports = {
+  hashDecode,
+  hashEncode,
 };
