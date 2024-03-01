9739d19cf9c4ebfc118734b66e32b815071d6257
Beterraba <ls6182315@gmail.com>
2024-03-01 20:58:15 -0300

    validate token middleware

---
+++ src/middlewares/token.middleware.js
@@ -0,0 +1,28 @@
+const UserUseCase = require("../useCases/user.usecase.js");
+
+const { tokenDecoder } = require("../utils/token.js");
+
+module.exports = class TokenValidator {
+  constructor() {
+    this.useCase = new UserUseCase();
+  }
+
+  async validateJWT(req, res, next) {
+    const token = req.headers.authorization;
+    if (!token) return res.status(401).json({ message: "Token not found" });
+
+    try {
+      const decoded = tokenDecoder(token);
+      const loggedUser = await this.useCase.findUserById(decoded.uuid);
+
+      if (!loggedUser) {
+        return res.status(403).json({ message: "Token must be a valid token" });
+      }
+
+      req.userId = loggedUser.uuid;
+      next();
+    } catch (error) {
+      return res.status(403).json({ message: "Token must be a valid token" });
+    }
+  }
+};
