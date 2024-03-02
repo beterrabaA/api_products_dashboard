7695ddbab06fb7b70991723b329b7a7fef919494
Beterraba <ls6182315@gmail.com>
2024-03-02 19:46:43 -0300

    add product usecase method

--- src/useCases/product.usecase.js
+++ src/useCases/product.usecase.js
@@ -1,17 +1,40 @@
 const { Product } = require("../models");
 const { tokenDecoder } = require("../utils/token");
 
+const { randomUUID } = require("crypto");
+
 module.exports = class ProductUseCase {
   constructor() {}
 
   async getAllProducts(token) {
     const decodedUser = tokenDecoder(token);
+
     const products = await Product.findAll({
-      where: { user_id: decodedUser.uuid },
+      where: { userId: decodedUser.id },
     });
 
     if (!products) throw new Error("products not found");
 
     return products;
+  }
+
+  async newProduct(token, product) {
+    console.log("foi");
+    const random = randomUUID();
+
+    const { name, brand, model, price, color } = product;
+    const decodedUser = tokenDecoder(token);
+    const stringProduct = JSON.stringify({ price, color });
+
+    const newProduct = await Product.create({
+      id: random,
+      name,
+      brand,
+      model,
+      data: [stringProduct],
+      userId: decodedUser.id,
+    });
+
+    return newProduct;
   }
 };
