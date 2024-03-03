2eb548c8b60350c6a3f80fa548f5b89bd26d352c
Beterraba <ls6182315@gmail.com>
2024-03-02 23:03:41 -0300

    fix sintaxe

--- src/middlewares/token.middleware.js
+++ src/middlewares/token.middleware.js
@@ -4,25 +4,27 @@ const { tokenDecoder } = require("../utils/token.js");
 
 module.exports = class TokenValidator {
   constructor() {
-    this.useCase = new UserUseCase();
+    this.usecase = new UserUseCase();
   }
 
   async validateJWT(req, res, next) {
-    const token = req.headers("Authorization");
+    const token = req.headers.authorization;
     if (!token) return res.status(401).json({ message: "Token not found" });
 
     try {
+      const usecase = new UserUseCase();
       const decoded = tokenDecoder(token);
-      const loggedUser = await this.useCase.findUserById(decoded.uuid);
+
+      const loggedUser = await usecase.findUserById(decoded.id);
 
       if (!loggedUser) {
         return res.status(403).json({ message: "Token must be a valid token" });
       }
 
-      req.userId = loggedUser.uuid;
+      req.userId = loggedUser.id;
       next();
     } catch (error) {
-      return res.status(403).json({ message: "Token must be a valid token" });
+      return res.status(403).json({ message: error.message });
     }
   }
 };
