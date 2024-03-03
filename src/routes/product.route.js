6d2f0c304ce5bf110943f7c88fb5c27cdd12276a
Beterraba <ls6182315@gmail.com>
2024-03-02 22:54:39 -0300

    create product router

---
+++ src/routes/product.route.js
@@ -0,0 +1,31 @@
+const { Router } = require("express");
+const ProductController = require("../controllers/product.controller.js");
+const JTWValidator = require("../middlewares/token.middleware.js");
+
+module.exports = class ProductRoute {
+  constructor() {
+    this.router = Router();
+    this.controller = new ProductController();
+    this.jwt = new JTWValidator();
+    this.initializeMiddlewares();
+    this.initRoutes();
+  }
+
+  initializeMiddlewares() {
+    this.router.use(this.jwt.validateJWT);
+  }
+
+  initRoutes() {
+    // get method "/api/product" route
+    this.router.get(
+      "/product",
+      this.controller.getAllProducts.bind(this.controller)
+    );
+
+    // post method "/api/product" route
+    this.router.post(
+      "/product",
+      this.controller.newProduct.bind(this.controller)
+    );
+  }
+};
